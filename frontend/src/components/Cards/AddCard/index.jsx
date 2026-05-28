70da079ca1de3e389cd954610db48d1621e2061b
Ygor Santos <santosygor2007@gmail.com>
2026-05-28 19:21:44 -0300

    refactor: reorganize inputs structure

--- frontend/src/components/Cards/AddCard/index.jsx
+++ frontend/src/components/Cards/AddCard/index.jsx
@@ -1,22 +1,24 @@
 import styled from "styled-components";
 import CardModel from "../CardModel/index.jsx";
-import { Input, InputSubmit } from "../../Inputs/index.jsx";
+import { Input, InputContainer, InputSubmit } from "../../Inputs/index.jsx";
+import Subtitle from "../Subtitle/index.jsx";
 
-const AddContainer = styled(CardModel)`
-    
-`
+const AddContainer = styled(CardModel)``;
 
-function AddCard(){
-    return (
-        <AddContainer titulo="Adicionar Filme">
-            <Input placeholder="Titulo"/>
-            <Input placeholder="Gênero"/>
-            <Input placeholder="Ano de Lançamento"/>
-            <Input placeholder="Duração em Minutos"/>
-            <Input placeholder="Diretor"/>
-            <InputSubmit value="Adicionar"/>
-        </AddContainer>
-    )
+function AddCard() {
+  return (
+    <AddContainer titulo="Adicionar Filme">
+      <Subtitle subtitle="Dados" />
+      <InputContainer>
+        <Input placeholder="Titulo" />
+        <Input placeholder="Gênero" />
+        <Input placeholder="Ano de Lançamento" />
+        <Input placeholder="Duração em Minutos" />
+        <Input placeholder="Diretor" />
+        <InputSubmit value="Adicionar" />
+      </InputContainer>
+    </AddContainer>
+  );
 }
 
 export default AddCard;
--- frontend/src/components/Cards/CardBody/index.jsx
+++ frontend/src/components/Cards/CardBody/index.jsx
@@ -5,27 +5,11 @@ const BodyContainer = styled.div`
   margin: 20px;
 `;
 
-const FiltersContainer = styled.div``;
-
-const FilterTitle = styled.h3`
-  color: #fff;
-  font-size: 22px;
-  font-weight: 100;
-`;
-
-const FilterContent = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 5px;
-`;
 
 function CardBody({ children }) {
   return (
     <BodyContainer>
-      <FiltersContainer>
-        <FilterTitle>Filtros</FilterTitle>
-        <FilterContent>{children}</FilterContent>
-      </FiltersContainer>
+      {children}
     </BodyContainer>
   );
 }
